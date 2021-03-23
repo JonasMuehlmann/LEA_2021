ed9df96f008aec0c297bfa46ead96dad7a495ad4
HollowProphet <49788305+HollowProphet@users.noreply.github.com>
2021-03-23 17:06:28 +0100

    Added ShadowOffset and MaxReflectionDepth constants

--- Backend/src/Constants.cs
+++ Backend/src/Constants.cs
@@ -2,8 +2,10 @@ namespace LEA_2021
 {
     public class Constants
     {
-        public static readonly string SceneDir     = "../../../../Backend/scenes";
-        public static readonly string MaterialsDir = SceneDir + "/materials";
-        public static readonly string OutputDir    = "../../../../Backend/out";
+        public static readonly string SceneDir           = "../../../../Backend/scenes";
+        public static readonly string MaterialsDir       = SceneDir + "/materials";
+        public static readonly string OutputDir          = "../../../../Backend/out";
+        public static readonly float  ShadowOffset       = 1e-4f;
+        public static readonly int    MaxReflectionDepth = 10;
     }
 }
